91cf88c870a6c1630edd91e9cd1884017c54de2b
Jihun Park <jihunpark.tech@gmail.com>
2023-12-29 22:16:21 +0900

    add: comment

--- src/main/java/com/myshop/lock/LockManager.java
+++ src/main/java/com/myshop/lock/LockManager.java
@@ -1,6 +1,13 @@
 package com.myshop.lock;
 
 public interface LockManager {
+    /**
+     *
+     * @param type 잠글 대상 타입 (domain.Article)
+     * @param id 식별자 (10)
+     * @return
+     * @throws LockException
+     */
     LockId tryLock(String type, String id) throws LockException;
 
     void checkLock(LockId lockId) throws LockException;
